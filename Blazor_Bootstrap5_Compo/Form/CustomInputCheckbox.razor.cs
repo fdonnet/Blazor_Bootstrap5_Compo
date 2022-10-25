c2d86f1a6981a3b5e350d26d34d81124844edb9d
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-10-25 09:59:46 +0200

    Input checkbox, switch mode

---
+++ Blazor_Bootstrap5_Compo/Form/CustomInputCheckbox.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_Bootstrap5_Compo.Form
+{
+    public partial class CustomInputCheckbox
+    {
+        [Parameter]
+        public string Id { get; set; } = "Id";
+        [Parameter]
+        public string Label { get; set; } = String.Empty;
+
+    }
+}
